9666dcedd1af640a215258580fbb4d69074459ba
LJM <lou@blocktimefinancial.com>
2023-05-27 13:22:49 +0000

    Added ContractAuth helpers

---
+++ samples/contractAuth.js
@@ -0,0 +1,81 @@
+const SorobanClient = require("soroban-client");
+const xdr = SorobanClient.xdr;
+
+function ContractAuth(
+  pk,
+  functionName,
+  nonce = "0",
+  args = [],
+  subInvocations = [],
+  signatureArgs = []
+) {
+  return new SorobanClient.xdr.ContractAuth({
+    addressWithNonce: AddressWithNonce(pk, nonce),
+    rootInvocation: AuthorizedInvocation(functionName, args, subInvocations),
+    signatureArgs: signatureArgs,
+  });
+}
+
+function AddressWithNonce(pk, nonce = "0") {
+  return new SorobanClient.xdr.AddressWithNonce({
+    address: SorobanClient.xdr.ScAddress.scAddressTypeAccount(
+      SorobanClient.xdr.PublicKey.publicKeyTypeEd25519(
+        SorobanClient.StrKey.decodeEd25519PublicKey(pk)
+      )
+    ),
+    nonce: SorobanClient.xdr.Uint64.fromString(nonce),
+  });
+}
+
+function AuthorizedInvocation(functionName, args = [], subInvocations = []) {
+  return new SorobanClient.xdr.AuthorizedInvocation({
+    contractId: Buffer.alloc(32),
+    functionName: Buffer.from(functionName),
+    args: [],
+    subInvocations: [],
+  });
+}
+
+function LedgerFootPrint(readOnly = [], readWrite = []) {
+  return new SorobanClient.xdr.LedgerFootprint({
+    readOnly: readOnly,
+    readWrite: readWrite,
+  });
+}
+
+function SorobanResource(
+  readOnly = [],
+  readWrite = [],
+  instructions = 0,
+  readBytes = 0,
+  writeBytes = 0,
+  extendedMetaDataSizeBytes = 0
+) {
+  return new SorobanClient.xdr.SorobanResource({
+    footprint: LedgerFootPrint(readOnly, readWrite),
+    instructions,
+    readBytes,
+    writeBytes,
+    extendedMetaDataSizeBytes,
+  });
+}
+
+function SorobanTransactionData( readOnly = [],
+    readWrite = [],
+    instructions = 0,
+    readBytes = 0,
+    writeBytes = 0,
+    extendedMetaDataSizeBytes = 0, refundableFee = "0", extensionPoint = 0) {
+    return new SorobanClient.xdr.SorobanTransactionData({
+        resources: SorobanResource(readOnly, readWrite, instructions, readBytes, writeBytes, extendedMetaDataSizeBytes),
+        refundableFee: SorobanClient.xdr.Int64.fromString(refundableFee),
+        ext: new SorobanClient.xdr.ExtensionPoint(extensionPoint)
+    });
+}
+
+module.exports.ContractAuth = ContractAuth;
+module.exports.AddressWithNonce = AddressWithNonce;
+module.exports.AuthorizedInvocation = AuthorizedInvocation;
+module.exports.LedgerFootPrint = LedgerFootPrint;
+module.exports.SorobanResource = SorobanResource;
+module.exports.SorobanTransactionData = SorobanTransactionData;
