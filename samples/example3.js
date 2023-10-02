def95935e2265965494244a54d3bbb257812ca47
MitchellBlocktime <mitchell@blocktimefinancial.com>
2023-10-02 19:29:34 +0000

    updated example3.js

--- samples/example3.js
+++ samples/example3.js
@@ -28,58 +28,6 @@ Contract Symbol: USDC
 
 //write a function to create a contract transaction
 
-function createContractTransaction(networkPassphrase, sourceAccount, contractId, method, ...args) {
-    //console.log(`Inside createContractTransaction with contract id: ${contractId}, method: ${method}`)
-    let myArgs = args || [];
-    const contract = new SorobanClient.Contract(contractId);
-
-    // console.log(
-    //     `Creating contract transaction for ${contractId} ${util.inspect(
-    //         sourceAccount,
-    //         false,
-    //         3,
-    //         true
-    //     )} ${method} ${myArgs}`
-    // );
-
-    return new SorobanClient.TransactionBuilder(sourceAccount, {
-        fee: 100,
-        networkPassphrase: networkPassphrase,
-    })
-        .addOperation(contract.call(method, ...myArgs))
-        .setTimeout(SorobanClient.TimeoutInfinite)
-        .build();
-}
-
-function authorizeInvocation(signer, networkPassphrase, invocation, ledgerValidityCount){
-    const networkId = hash (Buffer.from(networkPassphrase));
-    const nonce = randomInt(1, Number.MAX_SAFE_INTEGER);
-
-    const envelope = new xdr.HashIdPreimageSorobanAuthorization({
-        networkId, 
-        invocation, 
-        nonce: new xdr.Uint64(nonce), 
-        signatureExpirationLedger: ledgerValidityCount
-    });
-
-    const sig = signer.sign(hash(envelope.toXDR('raw')));
-
-    return new xdr.SorobanAuthorizationEntry({
-        credentials: xdr.SorobanCredentials.sorobanCredentialsAddress(
-            new xdr.SorobanAddressCredentials({
-                address: new Address(signer.publicKey()).toScAddress(),
-                nonce: envelope.nonce(),
-                signatureExpirationLedger: envelope.signatureExpirationLedger(),
-                signatureArgs: [nativeToScVal({
-                    'public_key': signer.rawPublicKey,
-                    'signature': sig,
-                })]
-            })
-        ),
-        rootInvocation: invocation,
-    })
-}
-
 async function invokeContract(params){
     console.log("Inside invokeContract")
     let body = params.body;
@@ -98,7 +46,7 @@ async function invokeContract(params){
 
     console.log("Simulating tx...")
     const sim = await server.simulateTransaction(tx)
-    console.log(`Sim: ${util.inspect(sim, false, 5)}`)
+    console.dir(sim);
 
     console.log("Prepping tx...")
     sim = await server.prepareTransaction(tx, "Test SDF Future Network ; October 2022");
@@ -149,13 +97,65 @@ async function invokeContract(params){
     return result;
 }
 
+function createContractTransaction(networkPassphrase, sourceAccount, contractId, method, ...args) {
+    console.log(`Inside createContractTransaction with contract id: ${contractId}, method: ${method}`)
+    let myArgs = args || [];
+
+    // console.log(
+    //     `Creating contract transaction for ${contractId} ${util.inspect(
+    //         sourceAccount,
+    //         false,
+    //         3,
+    //         true
+    //     )} ${method} ${myArgs}`
+    // );
+
+    return new SorobanClient.TransactionBuilder(sourceAccount, {
+        fee: 100,
+        networkPassphrase: networkPassphrase,
+        v1: true,
+    })
+        .addOperation(contractId.call(method, ...myArgs))
+        .setTimeout(SorobanClient.TimeoutInfinite)
+        .build();
+}
+
+function authorizeInvocation(signer, networkPassphrase, invocation, ledgerValidityCount){
+    const networkId = hash (Buffer.from(networkPassphrase));
+    const nonce = randomInt(1, Number.MAX_SAFE_INTEGER);
+
+    const envelope = new xdr.HashIdPreimageSorobanAuthorization({
+        networkId, 
+        invocation, 
+        nonce: new xdr.Uint64(nonce), 
+        signatureExpirationLedger: ledgerValidityCount
+    });
+
+    const sig = signer.sign(hash(envelope.toXDR('raw')));
+
+    return new xdr.SorobanAuthorizationEntry({
+        credentials: xdr.SorobanCredentials.sorobanCredentialsAddress(
+            new xdr.SorobanAddressCredentials({
+                address: new Address(signer.publicKey()).toScAddress(),
+                nonce: envelope.nonce(),
+                signatureExpirationLedger: envelope.signatureExpirationLedger(),
+                signatureArgs: [nativeToScVal({
+                    'public_key': signer.rawPublicKey,
+                    'signature': sig,
+                })]
+            })
+        ),
+        rootInvocation: invocation,
+    })
+}
 
 async function main() {
     const account = await server.getAccount(pk);
     // const contract = new SorobanClient.Contract(
     //     "e94760e06da32836fe8dcc71e7b33db0c5297a8b86ee2db0e23ea5e612353b19"
     // );
-    const contract = "CCT76EENITUCBWSZAGVW2NEQPDKJFMJFVFCYLEPOJPGGGWWJGTK7XUQE"
+    const testsc = "CCT76EENITUCBWSZAGVW2NEQPDKJFMJFVFCYLEPOJPGGGWWJGTK7XUQE"
+    const contract = new SorobanClient.Contract(testsc);
 
     // const params = {
     //     env: "test",
@@ -168,7 +168,7 @@ async function main() {
     //     admin:  "GBL74ETHLQJQUQW7YQT4KO3HJVR74TIHSBW6ENRBSFHUTATBRKKLGW4Y"
     // }
     const address1 = new SorobanClient.Address("CCT76EENITUCBWSZAGVW2NEQPDKJFMJFVFCYLEPOJPGGGWWJGTK7XUQE").toScVal();
-    const address2 = new SorobanClient.Address("GBL74ETHLQJQUQW7YQT4KO3HJVR74TIHSBW6ENRBSFHUTATBRKKLGW4Y").toScVal();
+    const address2 = new SorobanClient.Address("CCUVXXAFZ5UFVNBXTLFANY5M3OO4PV5MQ2PBTHLBPVQLFKN2AZ63KERY").toScVal();
     const address3 = new SorobanClient.Address("GBL74ETHLQJQUQW7YQT4KO3HJVR74TIHSBW6ENRBSFHUTATBRKKLGW4Y").toScVal();
 
     //console.log(address1)
